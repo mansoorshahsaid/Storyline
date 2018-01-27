3f2230b3bcb43537447d0a0a70ae8f33222cf3dc
Shameiz Rangwala <srangwal@edu.uwaterloo.ca>
2018-01-27 18:14:44 -0500

    finished profile

--- Storyline/ProfileVC.swift
+++ Storyline/ProfileVC.swift
@@ -10,16 +10,24 @@ import UIKit
 
 class ProfileVC: UIViewController {
 
+    @IBOutlet weak var ProfileStories: UITextField!
+    @IBOutlet weak var ProfileUpvotes: UITextField!
+    @IBOutlet weak var upImg: UIImageView!
     @IBOutlet weak var ProfileImg: UIImageView!
-    @IBOutlet weak var ProfileEmail: UITextField!
     @IBOutlet weak var ProfileName: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.ProfileImg.layer.cornerRadius = self.ProfileImg.frame.size.width/2;
+        //self.ProfileImg.layer.shadowColor = #colorLiteral(red: 0.009499914944, green: 0.00950041879, blue: 0.009500147775, alpha: 1)
+        //self.ProfileImg.layer.shadowRadius = 30
+        self.ProfileImg.layer.borderWidth = 1.0;
+        self.ProfileImg.layer.borderColor = #colorLiteral(red: 0.009499914944, green: 0.00950041879, blue: 0.009500147775, alpha: 1);
         self.ProfileImg.clipsToBounds = true
         ProfileName.text = "Shameiz Rangwala"
         ProfileName.borderStyle = UITextBorderStyle.none
+        self.ProfileUpvotes.text = "25"
+        self.ProfileStories.text = "33"
         // Do any additional setup after loading the view.
     }
 
