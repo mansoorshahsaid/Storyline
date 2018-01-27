50af629f1d17f96543fd19fcf2bd49655d1d90c7
Anthony <fioritoanthony@outlook.com>
2018-01-27 14:21:17 -0500

    UIWindow init

--- Storyline/AppDelegate.swift
+++ Storyline/AppDelegate.swift
@@ -13,7 +13,7 @@ import Firebase
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
+    var window: UIWindow? = UIWindow(frame: UIScreen.main.bounds)
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
